1cac98870fd7691e76b7490ca55b1b88c69a2e82
arfidorama24 <125841716+arfidorama24@users.noreply.github.com>
2023-02-27 10:34:32 +0700

    t

--- Impact/login.php
+++ Impact/login.php
@@ -7,7 +7,7 @@ error_reporting(0);
 session_start();
  
 if (isset($_SESSION['username'])) {
-    header("Location:index.html");
+    header("Location:users-profile.php");
 }
  
 if (isset($_POST['submit'])) {
@@ -19,7 +19,7 @@ if (isset($_POST['submit'])) {
     if ($result->num_rows > 0) {
         $row = mysqli_fetch_assoc($result);
         $_SESSION['username'] = $row['username'];
-        header("Location:index.html");
+        header("Location:users-profile.php");
     } else {
         echo "<script>alert('Email atau password Anda salah. Silahkan coba lagi!')</script>";
     }
@@ -40,6 +40,7 @@ if (isset($_POST['submit'])) {
     <title>WebPerhutani</title>
 </head>
 <body>
+    
     <div class="alert alert-warning" role="alert">
         <?php echo $_SESSION['error']?>
     </div>
